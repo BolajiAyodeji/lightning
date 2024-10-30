1a95eb68d245a3bf610c367c4097eb7223e84be5
Rory McKinley <rorymckinley@gmail.com>
2024-10-30 12:55:56 +0200

    Dumb down list_all in prep for other actor types

--- lib/lightning/auditing/audit.ex
+++ lib/lightning/auditing/audit.ex
@@ -119,7 +119,8 @@ defmodule Lightning.Auditing.Audit do
     field :actor_type, Ecto.Enum,
       values: [:project_repo_connection, :trigger, :user]
 
-    field :actor, :map, virtual: true
+    field :actor_display_identifier, :string, virtual: true
+    field :actor_display_label, :string, virtual: true
 
     timestamps(updated_at: false)
   end
